66e49a92d7f7fc81c51744480f8075a9dfa680ba
Abhinav Deshmukh <abhideshmukh9322@gmail.com>
2025-11-02 14:24:23 +0530

    resume update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/18CVf-3kYPHvU38YNEctuf-hgwu3w5UYJ/view?usp=sharing"
+            href="https://drive.google.com/file/d/1hPQXqluhfTA6cVo6tjEEI66YZXIRoZMu/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
